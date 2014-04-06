38e69183f2f29dda8dac22c939a815b3868432c5
tlongren <tlongren@gmail.com>
2014-04-06 12:20:08 -0500

    Add plugin.

---
+++ jquery.stickyalert.js
@@ -0,0 +1,36 @@
+(function($){
+
+  $.fn.extend({
+
+    stickyalert: function(options) {
+
+        var defaults = {
+            barColor: '#222',
+            barFontColor: '#FFF',
+            barText: 'I like to work! Hire me!',
+            barTextLink: 'http://longren.io/work-with-me/',
+        };
+
+        var options = $.extend(defaults, options);
+
+        return this.each(function() {
+
+          $('<div class="alert-box" style="background-color:' + options.barColor + '"><a href="' + options.barTextLink + '" style="color:' + options.barFontColor + '">' + options.barText + '</a><a href="" class="close">&#10006;</a></div>').appendTo(this);
+
+          $(".alert-box").delegate("a.close", "click", function(event) {
+
+            event.preventDefault();
+
+            $(this).closest(".alert-box").fadeOut(function(event){
+
+              $(this).remove();
+
+            });
+
+          });
+
+        });
+      }
+  });
+
+})(jQuery);
