1cea175065b03711f4921d06ad3ff30bc63daee3
tlongren <tlongren@gmail.com>
2014-04-09 20:07:47 -0500

    Add font size.

--- jquery.stickyalert.js
+++ jquery.stickyalert.js
@@ -16,7 +16,7 @@
 
         return this.each(function() {
 
-          $('<div class="alert-box" style="background-color:' + options.barColor + '"><a href="' + options.barTextLink + '" style="color:' + options.barFontColor + '">' + options.barText + '</a><a href="" class="close">&#10006;</a></div>').appendTo(this);
+          $('<div class="alert-box" style="background-color:' + options.barColor + '"><a href="' + options.barTextLink + '" style="color:' + options.barFontColor + '; font-size:' + options.barFontSize + '">' + options.barText + '</a><a href="" class="close">&#10006;</a></div>').appendTo(this);
 
           $(".alert-box").delegate("a.close", "click", function(event) {
 
