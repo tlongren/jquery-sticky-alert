f25c521529032b306ffdc864f9c0cbc55e2399f7
Tyler Longren <tlongren@gmail.com>
2014-09-15 08:55:37 -0500

    Now fadein.

--- jquery.stickyalert.js
+++ jquery.stickyalert.js
@@ -36,7 +36,7 @@
 
           else {
 
-          $('<div class="alert-box" style="background-color:' + options.barColor + '"><a href="' + options.barTextLink + '" style="color:' + options.barFontColor + '; font-size:' + options.barFontSize + '">' + options.barText + '</a><a href="" class="close">&#10006;</a></div>').appendTo(this).hide().delay(options.displayDelay);
+          $('<div class="alert-box" style="background-color:' + options.barColor + '"><a href="' + options.barTextLink + '" style="color:' + options.barFontColor + '; font-size:' + options.barFontSize + '">' + options.barText + '</a><a href="" class="close">&#10006;</a></div>').appendTo(this).hide().delay(options.displayDelay).fadeIn(slow);
 
 
             $(".alert-box").delegate("a.close", "click", function(event) {
